18e2390c025e98d17e3054eb167c71ed1c050a56
Rutam21 <rutamprita@gmail.com>
2022-10-17 14:04:18 +0530

    APPW-4366: Add Code Linter To utopia-php/audit

--- src/Audit/Audit.php
+++ src/Audit/Audit.php
@@ -11,7 +11,7 @@ use Utopia\Exception;
 
 class Audit
 {
-    const COLLECTION = "audit";
+    const COLLECTION = 'audit';
 
     /**
      * @var Database
@@ -19,7 +19,7 @@ class Audit
     private Database $db;
 
     /**
-     * @param Database $db
+     * @param  Database  $db
      */
     public function __construct(Database $db)
     {
@@ -28,8 +28,8 @@ class Audit
 
     public function setup(): void
     {
-        if (!$this->db->exists($this->db->getDefaultDatabase())) {
-            throw new Exception("You need to create the database before running Audit setup");
+        if (! $this->db->exists($this->db->getDefaultDatabase())) {
+            throw new Exception('You need to create the database before running Audit setup');
         }
 
         $attributes = [
@@ -147,14 +147,13 @@ class Audit
      *
      * Add specific event log
      *
-     * @param string $userId
-     * @param string $event
-     * @param string $resource
-     * @param string $userAgent
-     * @param string $ip
-     * @param string $location
-     * @param array  $data
-     *
+     * @param  string  $userId
+     * @param  string  $event
+     * @param  string  $resource
+     * @param  string  $userAgent
+     * @param  string  $ip
+     * @param  string  $location
+     * @param  array  $data
      * @return bool
      */
     public function log(string $userId, string $event, string $resource, string $userAgent, string $ip, string $location, array $data = []): bool
@@ -172,17 +171,17 @@ class Audit
                 'time' => DateTime::now(),
             ]));
         });
+
         return true;
     }
 
     /**
      * Get All Logs By User ID.
      *
-     * @param string $userId
-     * @param int $limit
-     * @param int $offset
-     * @param Document|null $orderAfter
-     *
+     * @param  string  $userId
+     * @param  int  $limit
+     * @param  int  $offset
+     * @param  Document|null  $orderAfter
      * @return array
      */
     public function getLogsByUser(string $userId, int $limit = 25, int $offset = 0, Document $orderAfter = null): array
@@ -202,14 +201,14 @@ class Audit
                 queries: $queries,
             );
         });
+
         return $result;
     }
 
     /**
      * Get Logs Count By User ID.
      *
-     * @param string $userId
-     *
+     * @param  string  $userId
      * @return int
      */
     public function countLogsByUser(string $userId): int
@@ -220,18 +219,17 @@ class Audit
                 queries: $this->buildQuery(['userId' => $userId], Query::TYPE_EQUAL)
             );
         });
+
         return $result;
     }
-
 
     /**
      * Get All Logs By Resource.
      *
-     * @param string $resource
-     * @param int $limit
-     * @param int $offset
-     * @param Document|null $orderAfter
-     *
+     * @param  string  $resource
+     * @param  int  $limit
+     * @param  int  $offset
+     * @param  Document|null  $orderAfter
      * @return array
      */
     public function getLogsByResource(string $resource, int $limit = 25, int $offset = 0, Document $orderAfter = null): array
@@ -251,14 +249,14 @@ class Audit
                 queries: $queries,
             );
         });
+
         return $results;
     }
 
     /**
      * Get Logs Count By Resource.
      *
-     * @param string $resource
-     *
+     * @param  string  $resource
      * @return int
      */
     public function countLogsByResource(string $resource): int
@@ -269,18 +267,18 @@ class Audit
                 queries: $this->buildQuery(['resource' => $resource], Query::TYPE_EQUAL)
             );
         });
+
         return $results;
     }
 
     /**
      * Get All Logs By User and Events.
      *
-     * @param string $userId
-     * @param array $events
-     * @param int $limit
-     * @param int $offset
-     * @param Document|null $orderAfter
-     *
+     * @param  string  $userId
+     * @param  array  $events
+     * @param  int  $limit
+     * @param  int  $offset
+     * @param  Document|null  $orderAfter
      * @return array
      */
     public function getLogsByUserAndEvents(string $userId, array $events, int $limit = 25, int $offset = 0, Document $orderAfter = null): array
@@ -303,15 +301,15 @@ class Audit
                 queries: $queries,
             );
         });
+
         return $results;
     }
 
     /**
      * Get Logs Count By User and Events.
      *
-     * @param string $userId
-     * @param array $events
-     *
+     * @param  string  $userId
+     * @param  array  $events
      * @return int
      */
     public function countLogsByUserAndEvents(string $userId, array $events): int
@@ -325,18 +323,18 @@ class Audit
                 ], Query::TYPE_EQUAL)
             );
         });
+
         return $results;
     }
 
     /**
      * Get All Logs By Resource and Events.
      *
-     * @param string $resource
-     * @param array $events
-     * @param int $limit
-     * @param int $offset
-     * @param Document|null $orderAfter
-     *
+     * @param  string  $resource
+     * @param  array  $events
+     * @param  int  $limit
+     * @param  int  $offset
+     * @param  Document|null  $orderAfter
      * @return array
      */
     public function getLogsByResourceAndEvents(string $resource, array $events, int $limit = 25, int $offset = 0, Document $orderAfter = null): array
@@ -359,15 +357,15 @@ class Audit
                 queries: $queries,
             );
         });
+
         return $results;
     }
 
     /**
      * Get All Logs By Resource and Events.
      *
-     * @param string $resource
-     * @param array $events
-     *
+     * @param  string  $resource
+     * @param  array  $events
      * @return int
      */
     public function countLogsByResourceAndEvents(string $resource, array $events): int
@@ -381,14 +379,14 @@ class Audit
                 ], Query::TYPE_EQUAL)
             );
         });
+
         return $results;
     }
 
     /**
      * Delete all logs older than $timestamp seconds
      *
-     * @param string $datetime
-     *
+     * @param  string  $datetime
      * @return bool
      */
     public function cleanup(string $datetime): bool
@@ -398,15 +396,16 @@ class Audit
                 $documents = $this->db->find(
                     collection: Audit::COLLECTION,
                     queries: $this->buildQuery([
-                        'time' => $datetime
+                        'time' => $datetime,
                     ], Query::TYPE_LESSER)
                 );
 
                 foreach ($documents as $document) {
                     $this->db->deleteDocument(Audit::COLLECTION, $document['$id']);
                 }
-            } while (!empty($documents));
+            } while (! empty($documents));
         });
+
         return true;
     }
 
@@ -416,25 +415,26 @@ class Audit
      *
      * The $method is applied to each k/v pair
      *
-     * @param array $values
-     * @param string $method
-     *
+     * @param  array  $values
+     * @param  string  $method
      * @return Query[]
+     *
      * @throws Exception
      */
     private function buildQuery(array $values, string $method): array
     {
-        if (!Query::isMethod($method)) {
+        if (! Query::isMethod($method)) {
             throw new Exception('Method not supported');
         }
 
         $query = [];
         foreach ($values as $key => $value) {
-            if (!\is_array($value)) {
+            if (! \is_array($value)) {
                 $value = [$value];
             }
             $query[] = new Query($method, $key, $value);
         }
+
         return $query;
     }
 }
