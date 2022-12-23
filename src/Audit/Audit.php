056ab018e9f5404441d8b98e3d50581e74a65c29
shimon <shimon@appwrite.io>
2022-12-23 11:40:28 +0200

    userInternalId addition

--- src/Audit/Audit.php
+++ src/Audit/Audit.php
@@ -34,6 +34,15 @@ class Audit
 
         $attributes = [
             new Document([
+                '$id' => 'userInternalId',
+                'type' => Database::VAR_STRING,
+                'size' => Database::LENGTH_KEY,
+                'required' => true,
+                'signed' => true,
+                'array' => false,
+                'filters' => [],
+            ]),
+            new Document([
                 '$id' => 'userId',
                 'type' => Database::VAR_STRING,
                 'size' => Database::LENGTH_KEY,
@@ -156,11 +165,12 @@ class Audit
      * @param  array  $data
      * @return bool
      */
-    public function log(string $userId, string $event, string $resource, string $userAgent, string $ip, string $location, array $data = []): bool
+    public function log(string $userInternalId, string $userId, string $event, string $resource, string $userAgent, string $ip, string $location, array $data = []): bool
     {
-        Authorization::skip(function () use ($userId, $event, $resource, $userAgent, $ip, $location, $data) {
+        Authorization::skip(function () use ($userId, $userInternalId, $event, $resource, $userAgent, $ip, $location, $data) {
             $this->db->createDocument(Audit::COLLECTION, new Document([
                 '$permissions' => [],
+                'userInternalId' => $userInternalId,
                 'userId' => $userId,
                 'event' => $event,
                 'resource' => $resource,
