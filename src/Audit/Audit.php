59f88d71f9d93603393aeda368a975b10b8ddb17
Matej Bačo <matejbaco2000@gmail.com>
2024-03-08 11:29:31 +0000

    Introduce auth instance

--- src/Audit/Audit.php
+++ src/Audit/Audit.php
@@ -17,10 +17,12 @@ class Audit
     public const COLLECTION = 'audit';
 
     private Database $db;
+    private Authorization $auth;
 
-    public function __construct(Database $db)
+    public function __construct(Database $db, Authorization $auth)
     {
         $this->db = $db;
+        $this->auth = $auth;
     }
 
     /**
@@ -34,7 +36,7 @@ class Audit
     public function setup(): void
     {
         if (! $this->db->exists($this->db->getDatabase())) {
-            throw new Exception('You need to create the database before running Audit setup');
+            throw new \Exception('You need to create the database before running Audit setup');
         }
 
         $attributes = [
@@ -170,7 +172,7 @@ class Audit
      */
     public function log(string $userId, string $event, string $resource, string $userAgent, string $ip, string $location, array $data = []): bool
     {
-        Authorization::skip(function () use ($userId, $event, $resource, $userAgent, $ip, $location, $data) {
+        $this->auth->skip(function () use ($userId, $event, $resource, $userAgent, $ip, $location, $data) {
             $this->db->createDocument(Audit::COLLECTION, new Document([
                 '$permissions' => [],
                 'userId' => $userId,
@@ -201,7 +203,7 @@ class Audit
     public function getLogsByUser(string $userId, ?int $limit = null, ?int $offset = null, ?Document $orderAfter = null): array
     {
         /** @var array<Document> $result */
-        $result = Authorization::skip(function () use ($userId, $limit, $offset, $orderAfter) {
+        $result = $this->auth->skip(function () use ($userId, $limit, $offset, $orderAfter) {
             $queries[] = Query::equal('userId', [$userId]);
             $queries[] = Query::orderDesc('');
 
@@ -233,7 +235,7 @@ class Audit
     public function countLogsByUser(string $userId): int
     {
         /** @var int $count */
-        $count = Authorization::skip(function () use ($userId) {
+        $count = $this->auth->skip(function () use ($userId) {
             return $this->db->count(
                 collection: Audit::COLLECTION,
                 queries: [Query::equal('userId', [$userId])]
@@ -257,7 +259,7 @@ class Audit
     public function getLogsByResource(string $resource, ?int $limit = 25, ?int $offset = null, ?Document $orderAfter = null): array
     {
         /** @var array<Document> $result */
-        $result = Authorization::skip(function () use ($resource, $limit, $offset, $orderAfter) {
+        $result = $this->auth->skip(function () use ($resource, $limit, $offset, $orderAfter) {
             $queries[] = Query::equal('resource', [$resource]);
             $queries[] = Query::orderDesc('');
 
@@ -291,7 +293,7 @@ class Audit
     public function countLogsByResource(string $resource): int
     {
         /** @var int $count */
-        $count = Authorization::skip(function () use ($resource) {
+        $count = $this->auth->skip(function () use ($resource) {
             return $this->db->count(
                 collection: Audit::COLLECTION,
                 queries: [Query::equal('resource', [$resource])]
@@ -316,7 +318,7 @@ class Audit
     public function getLogsByUserAndEvents(string $userId, array $events, ?int $limit = null, ?int $offset = null, ?Document $orderAfter = null): array
     {
         /** @var array<Document> $result */
-        $result = Authorization::skip(function () use ($userId, $events, $limit, $offset, $orderAfter) {
+        $result = $this->auth->skip(function () use ($userId, $events, $limit, $offset, $orderAfter) {
             $queries[] = Query::equal('userId', [$userId]);
             $queries[] = Query::equal('event', $events);
             $queries[] = Query::orderDesc('');
@@ -352,7 +354,7 @@ class Audit
     public function countLogsByUserAndEvents(string $userId, array $events): int
     {
         /** @var int $count */
-        $count = Authorization::skip(function () use ($userId, $events) {
+        $count = $this->auth->skip(function () use ($userId, $events) {
             return $this->db->count(
                 collection: Audit::COLLECTION,
                 queries: [
@@ -380,7 +382,7 @@ class Audit
     public function getLogsByResourceAndEvents(string $resource, array $events, ?int $limit = null, ?int $offset = null, ?Document $orderAfter = null): array
     {
         /** @var array<Document> $result */
-        $result = Authorization::skip(function () use ($resource, $events, $limit, $offset, $orderAfter) {
+        $result = $this->auth->skip(function () use ($resource, $events, $limit, $offset, $orderAfter) {
             $queries[] = Query::equal('resource', [$resource]);
             $queries[] = Query::equal('event', $events);
             $queries[] = Query::orderDesc('');
@@ -416,7 +418,7 @@ class Audit
     public function countLogsByResourceAndEvents(string $resource, array $events): int
     {
         /** @var int $count */
-        $count = Authorization::skip(function () use ($resource, $events) {
+        $count = $this->auth->skip(function () use ($resource, $events) {
             return $this->db->count(
                 collection: Audit::COLLECTION,
                 queries: [
@@ -440,7 +442,7 @@ class Audit
      */
     public function cleanup(string $datetime): bool
     {
-        Authorization::skip(function () use ($datetime) {
+        $this->auth->skip(function () use ($datetime) {
             do {
                 $documents = $this->db->find(
                     collection: Audit::COLLECTION,
